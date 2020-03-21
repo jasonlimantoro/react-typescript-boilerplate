02d0b9995a756cb679f4d0c2d4c4ccd29ab9a8a6
jasonlimantoro <jasonlimantoro99@gmail.com>
2020-03-21 11:40:13 +0800

    [TYPESCRIPT]: add support for absolute import from src/ folder

--- src/modules/todo/selector.ts
+++ src/modules/todo/selector.ts
@@ -1,5 +1,5 @@
 import { createSelector } from "reselect";
-import { AppState } from "../types";
+import { AppState } from "src/modules/types";
 
 const selectTodoDetail = (state: AppState) => state.todo.todos;
 const selectTodoIds = (state: AppState) => state.todo.ids;
